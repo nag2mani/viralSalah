e53bf75078e8b9e6ce63c726c82be389546b21a6
nag2mani <nag2mani@gmail.com>
2025-04-26 15:01:44 +0530

    added dashboard page

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+// This code is inspired from EdRoh's github:https://github.com/ed-roh/react-admin-dashboard
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
